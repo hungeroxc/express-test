393ef96a594fc710d061f06fca579158caeda6b8
oxc <oxc@oxc.com>
2018-06-28 08:25:06 +0800

    up 支持正则路由和路由参数

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ const express = require('express')
 
 const app = express()
 
+// 固定路由
 app.get('/', (req, res) => {
-    res.send('home page')
+    const json = {
+        name: 'oxc',
+        age: 25
+    }
+    res.send(json)
+})
+
+// 变化路由
+app.get('/profile/:id', (req, res) => {
+    res.send(`profile page ${req.params.id}`)
+})
+
+// 正则匹配路由
+app.get('/reg/a?:b', (req, res) => {
+    res.send('abcd')
 })
 
 app.listen(3000)
