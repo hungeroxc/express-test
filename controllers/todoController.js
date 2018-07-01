5b84c42962be2664dabb7f3a98a5c56e67da651b
oxc <oxc@oxc.com>
2018-07-01 17:32:38 +0800

    up 引入线上数据库

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,35 +1,37 @@
 const bodyParser = require('body-parser')
 const urlencodeParser = bodyParser.urlencoded({extended: false})
-
-let data = [
-    {
-        item: 'get milk'
-    },
-    {
-        item: 'walk dog'
-    },
-    {
-        item: 'kick some coding ass'
-    }
-]
+// 因为mongoose并且链接
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://hungeroxc:oxc19930221@ds223161.mlab.com:23161/hungeroxctodo')
+// 定义一个schema
+const todoSchema = new mongoose.Schema({
+    item: String
+})
+// 定义model
+const Todo = mongoose.model('Todo', todoSchema)
 
 module.exports = app => {
     app.get('/todo', (req, res) => {
-        res.render('todo', {todos: data})
+        Todo.find({}, (err, data) => {
+            if(err) throw err
+            res.render('todo', {todos: data})
+        })
     })
 
     app.post('/todo', urlencodeParser, (req, res) => {
-        data.push(req.body)
-        res.json(data)
+        const item = Todo(req.body).save((err, data) => {
+            if(err) throw err
+            res.json(data)
+        })
     })
 
     app.delete('/todo/:item', (req, res) => {
         
-        data = data.filter(todo => {
-            const list = todo.item.replace(/ /g, "-") !== req.params.item
-            return list
+        Todo.find({item: req.params.item.replace(/-/g, ' ')})
+        .remove((err, data) => {
+            if(err) throw err
+            res.json(data)
         })
-        res.json(data)
     })
 } 
 
